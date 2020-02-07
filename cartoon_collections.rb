d0c44298ca183a22cc9390ab4457707d3783e7f4
William Lin <willlin1996@gmail.com>
2020-02-07 03:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(foods)
-  foods.include?(cheese_types[0]) 
+  foods.include?(cheese_types) 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
